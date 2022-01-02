255805f8db012378d1faee41a3426a3722034f95
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-02 16:45:01 +0600

    LeetCode - Pairs of Songs with total durations divisible by 60"

---
+++ leetcode/pairsOfSongs.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+
+/**
+ * @param {number[]} time
+ * @return {number}
+ */
+ var numPairsDivisibleBy60 = function(time) {
+    let count = 0;
+    for(let i = 0; i < time.length; i++) {
+        for(let j = i+1; j < time.length; j++) {
+            if((time[i] + time[j]) % 60 === 0) count++;
+        }
+    }
+
+    return count;
+};
+
+
+console.log(numPairsDivisibleBy60([60, 60, 60]))
