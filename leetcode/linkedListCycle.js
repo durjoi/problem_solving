7ea779c58d68e5772e5aa7c0094e9e6eed6d7c39
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-05 15:56:04 +0600

    some data updated

--- leetcode/linkedListCycle.js
+++ leetcode/linkedListCycle.js
@@ -23,4 +23,17 @@
     return false;
 };
 
-hasCycle()
+// Floyd's Tortoise & Hare
+
+var hasCycle = function(head) {
+    let fast = head;
+    let slow = head;
+    while(fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if(fast === slow) return true;
+    }
+
+    return false;
+}
