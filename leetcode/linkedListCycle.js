bed3db87a7db146627185b872d64fdaa55fbe0d5
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-05 12:30:26 +0600

    LeetCode - Linked list cycle

---
+++ leetcode/linkedListCycle.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var hasCycle = function(head) {
+    let set = new Set();
+    while(head) {
+        if(set.has(head)) return true;
+        set.add(head);
+        head = head.next;
+    }
+
+    return false;
+};
+
+hasCycle()
