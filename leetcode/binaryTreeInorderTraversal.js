e8687dfb75b46606bb48e635bf3dfe0eaa15fcd8
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-08 18:40:24 +0600

    LeetCode - Binary Tree Inorder Traversal - Iterative Way

--- leetcode/binaryTreeInorderTraversal.js
+++ leetcode/binaryTreeInorderTraversal.js
@@ -12,6 +12,45 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
- var inorderTraversal = function(root) {
-    
+
+// Recursive Way
+
+const traversTree = (root, ans) => {
+    if(root === null) return null;
+    traversTree(root.left, ans);
+    ans.push(root.val);
+    traversTree(root.right, ans);
+}
+
+var inorderTraversal = function(root) {
+    let ans = [];
+    traversTree(root, ans);
+
+    return ans;
 };
+
+
+// Iterative Way 
+var inorderTraversal = function(root) {
+    let ans = [];
+    let stack = [];
+    if(root === null) return ans;
+    
+    stack.push(root);
+    
+    while(stack.length > 0) {
+        let current = stack[stack.length - 1];
+        
+        if(current.left != null) {
+            stack.push(current.left);
+            current.left = null;
+        } 
+        else {
+            ans.push(current.val);
+            stack.pop();
+            if(current.right) stack.push(current.right);
+        }
+    }
+
+    return ans;
+}
