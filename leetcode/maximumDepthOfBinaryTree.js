e65e66b2c85267142cf049895d53ea50d6d52eb2
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-10 11:37:53 +0600

    LeetCode - Maximum Depth of Binary Tree

---
+++ leetcode/maximumDepthOfBinaryTree.js
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    let maxLevel = 0; 
+    
+    const BFS = (node, level) => {
+        if(node === null) return;
+        
+        if(level > maxLevel) maxLevel = level;
+        BFS(node.left, level+1)
+        BFS(node.right, level+1)
+    }
+    
+    BFS(root, 1);
+    
+    return maxLevel;
+};
