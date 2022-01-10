c39232bb9fdcb6ef4d74e370ca79f543fa89008d
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-10 13:38:25 +0600

    LeetCode - Maximum Depth of Binary Tree - updated

--- leetcode/maximumDepthOfBinaryTree.js
+++ leetcode/maximumDepthOfBinaryTree.js
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
 
 /**
  * Definition for a binary tree node.
