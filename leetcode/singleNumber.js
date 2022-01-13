1f76e7e9b902816e53f5f384652368be55d883a0
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-01-13 10:45:15 +0600

    LeetCode - Single Number

---
+++ leetcode/singleNumber.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/single-number/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    let x = 0;
+    for(let num of nums) x = x ^ num;
+    
+    return x;
+    
+};
