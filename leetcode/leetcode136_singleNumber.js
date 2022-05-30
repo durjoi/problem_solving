22ebcdc2cea0032943e30d4a61a975d40158a57d
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2022-05-30 08:56:10 +0600

    leetcode136 Optimized solution added

--- leetcode/leetcode136_singleNumber.js
+++ leetcode/leetcode136_singleNumber.js
@@ -5,9 +5,11 @@
  * @return {number}
  */
  var singleNumber = function(nums) {
-    let x = 0;
-    for(let num of nums) x = x ^ num;
+    // let x = 0;
+    // for(let num of nums) x = x ^ num;
     
-    return x;
+    // return x;
+     
+     nums.reduce((acc, cur) => (acc ^= cur), 0);
     
 };
